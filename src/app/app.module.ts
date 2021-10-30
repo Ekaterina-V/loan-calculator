73b26d74bdfb0c1a34c210244807d5c95cebc66e
Ekaterina Voronina <yekaterina.voronina@gmail.com>
2021-10-30 19:05:48 +0200

    Create loan-calculator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoancalculatorComponent } from './loancalculator/loancalculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoancalculatorComponent
   ],
   imports: [
     BrowserModule
