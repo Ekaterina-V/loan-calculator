143d0a1a9fbb19b8a57825d4cbce8db597fdc042
Ekaterina Voronina <yekaterina.voronina@gmail.com>
2021-10-30 23:02:34 +0200

    Build input validation

--- src/app/loancalculator/loancalculator.component.ts
+++ src/app/loancalculator/loancalculator.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-loancalculator',
@@ -8,16 +9,46 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class LoancalculatorComponent implements OnInit {
   loancalculatorForm = new FormGroup({
-    monthlyIncome: new FormControl(''),
-    requestedAmount: new FormControl(''),
-    loanTerm: new FormControl(''),
-    children: new FormControl(''),
-    coapplicant: new FormControl(''),
+    monthlyIncome: new FormControl('', [
+      Validators.required,
+      Validators.min(500000),
+    ]),
+    requestedAmount: new FormControl('', [
+      Validators.required,
+      Validators.min(20000000),
+    ]),
+    loanTerm: new FormControl('', [
+      Validators.required,
+      Validators.min(36),
+      Validators.max(360),
+    ]),
+    children: new FormControl('', Validators.required),
+    coapplicant: new FormControl('', Validators.required),
   });
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  get monthlyIncome() {
+    return this.loancalculatorForm.get('monthlyIncome');
+  }
+
+  get requestedAmount() {
+    return this.loancalculatorForm.get('requestedAmount');
+  }
+
+  get loanTerm() {
+    return this.loancalculatorForm.get('loanTerm');
+  }
+
+  get children() {
+    return this.loancalculatorForm.get('children');
+  }
+
+  get coapplicant() {
+    return this.loancalculatorForm.get('coapplicant');
+  }
 
   onSubmit() {
     console.warn(this.loancalculatorForm.value);
