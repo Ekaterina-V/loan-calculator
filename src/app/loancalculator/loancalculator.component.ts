8d8f92f7186ad1bbb1bd0b7636f24165602b7c87
Ekaterina Voronina <yekaterina.voronina@gmail.com>
2021-10-30 19:50:33 +0200

    Create the FormGroup instance

--- src/app/loancalculator/loancalculator.component.ts
+++ src/app/loancalculator/loancalculator.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-loancalculator',
   templateUrl: './loancalculator.component.html',
-  styleUrls: ['./loancalculator.component.css']
+  styleUrls: ['./loancalculator.component.css'],
 })
 export class LoancalculatorComponent implements OnInit {
+  loancalculatorForm = new FormGroup({
+    monthlyIncome: new FormControl(''),
+    requestedAmount: new FormControl(''),
+    loanTerm: new FormControl(''),
+    children: new FormControl(''),
+    coapplicant: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
