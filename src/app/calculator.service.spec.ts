907de1de267f9f72e872f74cfe9328f3b83d3965
Ekaterina Voronina <yekaterina.voronina@gmail.com>
2021-11-01 21:23:47 +0100

    Make tests up

--- src/app/calculator.service.spec.ts
+++ src/app/calculator.service.spec.ts
@@ -1,12 +1,18 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { CalculatorService } from './calculator.service';
 
 describe('CalculatorService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
   let service: CalculatorService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
     service = TestBed.inject(CalculatorService);
   });
 
--- src/app/loancalculator/loancalculator.component.spec.ts
+++ src/app/loancalculator/loancalculator.component.spec.ts
@@ -1,22 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoancalculatorComponent } from './loancalculator.component';
+import { CalculatorService } from '../calculator.service';
+
+class MockCalculatorService {}
 
 describe('LoancalculatorComponent', () => {
   let component: LoancalculatorComponent;
   let fixture: ComponentFixture<LoancalculatorComponent>;
 
+  let calculatorService: CalculatorService;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoancalculatorComponent ]
-    })
-    .compileComponents();
+      declarations: [LoancalculatorComponent],
+      imports: [ReactiveFormsModule],
+      providers: [
+        { provide: CalculatorService, useValue: MockCalculatorService },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoancalculatorComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
